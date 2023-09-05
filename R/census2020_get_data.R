0c9c95976a1b356b41e50dcddf0ae7973fa78952
Mark Corrales <corrales.mark@epa.gov>
2023-09-05 10:26:33 -0400

    added block_radius_miles column to blockwts, redid all data.tables; improved functions
    block_radius_miles allows using 90% of that as a realistic distance to average resident within a block in cases where distance to internal point of block is smaller than this effective radius as done in EJScreen proximity scores.

--- R/census2020_get_data.R
+++ R/census2020_get_data.R
@@ -1,29 +1,42 @@
 #' Download and clean up block data census 2020
-#'
-#' @param folder path for downloaded files
-#' @param folderout path for assembled results files
-#' @param mystates default is DC and the 50 states, not PR not VI/GU/MU/AS
+#' 
+#' @param folder path for downloaded files, or you could try setting it to tempdir()
+#' @param folderout path for assembled results files, or you could try setting it to tempdir()
+#' @param mystates default is DC and the 50 states -- lacks PR and VI/GU/MU/AS
+#'   and probably would need those from a different source 
+#' @param do_download whether to do census2020_download()
+#' @param do_unzip whether to do census2020_unzip()
+#' @param do_read whether to do census2020_read()
+#' @param do_clean whether to do census2020_clean()
 #' @param ... passed to census2020_read()
 #'
-#' @return a data.table of US Census blocks with columns like
-#'   blockid lat lon pop area (area in square meters I think)
+#' @return invisibly returns a data.table of US Census blocks with columns like
+#'   blockid lat lon pop area (area in square meters)
 #' @export
 #'
 census2020_get_data <- function(folder = '~/../Downloads/census2020zip', 
                                        folderout = '~/../Downloads/census2020out', 
-                                       mystates = c(state.abb, 'DC'),
+                                       mystates = c(state.abb, 'PR', 'DC'),
+                                do_download=TRUE, do_unzip=TRUE, do_read=TRUE, do_clean=TRUE,
                                        ...) {
    # 'PR' data not in that same place!! VI GU MU AS too.
   
-  census2020_download(      folder = folder,    mystates = mystates) #  
+  if (do_download) {
+    census2020_download(      folder = folder,    mystates = mystates) #  
+  }
+  if (do_unzip) {
   census2020_unzip(         folder = folder,    mystates = mystates, folderout = folderout)
+  }
+  if (do_read) {
   blocks <- census2020_read(folder = folderout, mystates = mystates, ...) # not yet a data.table
+  }
+  if (do_clean) {
   blocks <- census2020_clean(blocks) # returns a data.table
+  }
+  cat('
+      To create the datasets, try something like 
+      census2020_save_datasets(blocks, etc etc) \n')
   
-  cat('To create the datasets, try census2020_save_datasets(blocks, usethis=TRUE, overwrite=TRUE) \n')
-  
-  # x <- census2020_save_datasets(x = blocks, 
-  #                         usethis=usethis, overwrite=overwrite, 
-  #                         metadata=metadata) # not tested.  
+  # x <- census2020_save_datasets(x = blocks, etc etc) # not tested.  
   invisible(blocks)
 }
